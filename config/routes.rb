49194bf090a61dd5f607b41ecf197e527666608c
yash <yash.c@simformsolutions.com>
2023-01-20 16:59:05 +0530

    TE7-T115:rails-practice:fix:controller rename is done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root "home#index"
-  get "/home", to: "home#index"
+  root "homes#index"
+  #get "/home", to: "homes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get home_index_url
     assert_response :success
