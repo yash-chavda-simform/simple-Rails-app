584473eba3b6378a3ac641686064a2276a8aeb78
yash <yash.c@simformsolutions.com>
2023-02-06 18:38:53 +0530

    TE7-T119:rails-practice:feat:callbacks

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -4,5 +4,41 @@ class Faculty < ApplicationRecord
   validates :phone_no , format:{ with:PHONE_REGEX}
   validates :email, uniqueness: true
   validates :birth_date, comparison: {less_than: Date.today, message:"birthdate can't be in future"}
-  validates :designation, inclusion: { in: %w(Ass.Prof. Prof.), message:"designation can't be %{value}"}, exclusion: { in: %w(HOD Sr.Prof.), message:"designation can't be HOD or Sr.Prof"} 
+  validates :designation, inclusion: { in: %w(Ass.Prof. Prof.), message:"designation can't be %{value}"}, exclusion: { in: %w(HOD Sr.Prof.), message:"designation can't be HOD or Sr.Prof"}
+  # validates :designation,  
+
+  #callbacks
+  after_validation :set_message, on: [ :create, :update]
+  # before_validation :check_date
+  after_destroy :confirm_delete
+  after_validation :validate_email, on: [:create]
+  after_initialize do |faculty|
+    puts "You have initialized an object!"
+  end
+  after_find do |faculty|
+    puts "You have found an object!"
+  end
+
+  private
+    def set_message
+      puts("Date Of Birth Is valid")
+    end
+  
+    def check_date
+      if birth_date.nil?
+        self.birth_date = 01/02/2000
+      end
+    end
+
+    def validate_email
+      puts("Email validated")
+    end
+
+    def update_record
+      puts("record updated")
+    end
+     
+    def confirm_delete
+      puts "record is deleted"
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,23 @@ class Student < ApplicationRecord
   validates :birth_date, comparison: {less_than: Date.today, message:"birthdate can't be in future"}
   validates :term_and_condition, acceptance: { message: 'You cannot proceed without accepting Terms of Usage' }
   validates :department, inclusion: { in: %w(IT CE), message:"department can't be %{value}"}
+  #callbacks
+  # before_validation :check_date
+  after_validation :validate_email, on: [:create]
+  after_destroy :print_message
+  before_destroy :print_before
+  private
+ 
+
+  def validate_email
+    puts("Email validated")
+  end
+  
+  def print_message
+    puts("data is deleted")
+  end 
+
+  def print_before
+    puts("data is going to deleted")
+  end
 end
