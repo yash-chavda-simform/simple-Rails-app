cd37153e030663aea2de791fc7e31c0689fe3041
yash <yash.c@simformsolutions.com>
2023-02-07 12:12:00 +0530

    TE7-T118:rails-practice:fix:done changes in model

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,10 +1,8 @@
 class Faculty < ApplicationRecord
   PHONE_REGEX = /\A\d{10}\z/
-  validates :last_name, :first_name, :birth_date, :email, :designation, presence: true
-  # validates :phone_no , length:{ is:10 }
+  validates :last_name, :first_name, :birth_date, :email, :designation, :phone_no,  presence: true
   validates :phone_no , format:{ with:PHONE_REGEX}
   validates :email, uniqueness: true
   validates :birth_date, comparison: {less_than: Date.today, message:"birthdate can't be in future"}
-  validates :designation, inclusion: { in: %w(Ass.Prof. Prof.), message:"designation can't be %{value}"}, exclusion: { in: %w(HOD Sr.Prof.), message:"designation can't be HOD or Sr.Prof"}
-  # validates :designation,  
+  validates :designation, inclusion: { in: %w(Ass.Prof. Prof.), message:"designation can't be %{value}"}, exclusion: { in: %w(HOD Sr.Prof.), message:"designation can't be HOD or Sr.Prof"} 
 end
